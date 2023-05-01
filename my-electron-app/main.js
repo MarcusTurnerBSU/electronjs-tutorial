cba96138176d0f88d527d714234410517d37c0c3
Marcus Turner <marcusjturner95@gmail.com>
2023-05-01 17:02:03 +0100

    completed part 3
    Using Preload Scripts

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,11 +1,15 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
-
+  ipcMain.handle("ping", () => "pong");
   win.loadFile("index.html");
 };
 
@@ -17,7 +21,8 @@ app.whenReady().then(() => {
   });
 });
 
-app.on("window-all-closed"),
-  () => {
-    if (process.platform !== "darwin") app.quit();
-  };
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
